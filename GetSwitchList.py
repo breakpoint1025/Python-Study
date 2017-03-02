805412986b4e7a6403052bd61f47e14f1ef2edbf
Colin Liu <breakpoint1025@gmail.com>
2017-03-02 22:44:47 +0800

    get all devices, save into file

--- GetSwitchList.py
+++ GetSwitchList.py
@@ -3,20 +3,33 @@ import re
 import time
 import os
 
-working_dr = r"E:\SNMPUpdate\SimpleSoftConfig\map"
+#working_dr = r"E:\SNMPUpdate\SimpleSoftConfig\map"
+working_dr = r"C:\GitHub\TestData\SimpleSoftConfig\map"
 #开始计时
 start = time.clock()
 
 saved_path = os.getcwd()
 os.chdir(working_dr)
 
+devinfo_list = []
+fo = open("devices_list.txt", "w+")
 for filename in os.listdir():
     f = open(filename, 'r')
     for line in f.readlines():
         if line.find('Name') != -1:
-            a = line.find('\"')
-            b = line.rfind('\"')
-            ip = line[line.find('\"')+1:-line.rfind('\"')]
+            devinfo = ""
+            devinfo = line[line.find('\"')+1:line.rfind('\"')]
+        if line.find('ReadCommunity') != -1:
+            devinfo = devinfo + ":" + line[line.find('\"')+1:line.rfind('\"')]
+        if line.find('WriteCommunity') != -1:
+            devinfo = devinfo + ":" + line[line.find('\"')+1:line.rfind('\"')]
+            devinfo_list.append(devinfo)
+            fo.write(devinfo + "\n")
+            print(devinfo)
+
+print("We have [%d] simulators" %(len(devinfo_list)))
+fo.close()
+        
 
 
 
