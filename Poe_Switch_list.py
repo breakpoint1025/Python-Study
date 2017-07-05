f75c3da94b8d0ea35a6a093198edbe255037d3d1
Colin Liu <colinliu@commscope.com>
2017-07-05 10:25:46 +0800

    update for unicode error

--- Poe_Switch_list.py
+++ Poe_Switch_list.py
@@ -4,11 +4,12 @@ import os
 import random
 import shutil
 import pyodbc
+import codecs
 
 
 
 
-def deal_walkfiles(root_dir, filestream, cursor):
+def deal_walkfiles(root_dir, poe_dir, filestream, cursor):
     file_list = os.listdir(root_dir)
     for f in file_list:
         file_path = os.path.join(root_dir, f)
@@ -25,18 +26,19 @@ def deal_walkfiles(root_dir, filestream, cursor):
                         cursor.execute("select Model, ManufacturerID, AlgorithmNumber from ProprietarySwitchInfo where oid = '%s'" %(oid))
                         row = cursor.fetchone()
                         if row:
-                            devinfo = oid + ","  + row.Model + "," + str(row.ManufacturerID) + "," + str(row.AlgorithmNumber) + "," + "false"
+                            devinfo = file_path + "," + oid + ","  + row.Model + "," + str(row.ManufacturerID) + "," + str(row.AlgorithmNumber) + "," + "false"
                         else:
-                            devinfo = oid + ","  + "" + "," + "" + "," + "" + "," + "false"
+                            devinfo = file_path + "," + oid + ","  + "" + "," + "" + "," + "" + "," + "false"
 
                     if (line.find('1.3.6.1.2.1.105.1.1.1.10') != -1) and (devinfo != ""):
                         devinfo = devinfo.replace("false", "true", 1)
                         break
                 if len(devinfo) > 0:
-                    fo.write(devinfo + "\n")
                     print(devinfo)
+                    fo.write(devinfo + "\n")
+
         elif os.path.isdir(file_path):
-            deal_walkfiles(file_path, filestream, cursor)
+            deal_walkfiles(file_path, poe_dir, filestream, cursor)
 
 
 
@@ -44,13 +46,19 @@ def deal_walkfiles(root_dir, filestream, cursor):
 if __name__ == '__main__':
 
     connStr = 'DRIVER={SQL Server};SERVER=192.168.200.246\SMSQL2008;DATABASE=SNMPSiteData;UID=sa;PWD=SM7sm7!@#$%12345'
+    poe_switches_dir = r"E:\walkfiles"
+    if os.path.isdir(poe_switches_dir):
+        pass
+    else:
+        os.mkdir(poe_switches_dir)
     saved_path = os.getcwd()
     dir = os.path.dirname(os.path.abspath(__file__))
     cnxn = pyodbc.connect(connStr)
     cursor = cnxn.cursor()
-    fo = open(".\switch_PoE_list.csv", "w+")
+    fo = codecs.open(".\switch_PoE_list.csv", "w+", "utf-8")
     fo.write("OID,Switch Model, ManufacturerID, AlgorithmNumber" + "\n")
-    deal_walkfiles(dir, fo, cursor)
+    deal_walkfiles(dir, poe_switches_dir, fo, cursor)
+    #deal_walkfiles(r"E:\SM7\Git\SNMPWalkfiles\GitD4S\WalkfileByCode\7.1.2M&7.2.1C", poe_switches_dir, fo, cursor)
     fo.close()
     cnxn.close()
     print("Get PoE switch list done!")
