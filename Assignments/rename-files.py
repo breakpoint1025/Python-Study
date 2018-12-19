714af8ad2fb87cfff3a3d56ade3b17712247c73b
Colin Liu <colinliu@commscope.com>
2018-12-19 16:05:38 +0800

    for_loop_test

--- Assignments/rename-files.py
+++ Assignments/rename-files.py
@@ -1,18 +1,22 @@
 import os
 
 def rename_files():
-	files = os.listdir(r"C:\GitHub\FreeCodeCamp-Study")
+	files = os.listdir(r"E:\VPC")
 	print(files)
 	for file in files:
 		print(file)
 
-
-
 def walk_files():
-	for root, dirs, files in os.walk(r"C:\GitHub\FreeCodeCamp-Study"):
+	for root, dirs, files in os.walk(r"E:\VPC"):
 		for file in files:
 			print(os.path.join(root,file))
+
+def for_loop_test():
+	for i, element in zip([1,2,3], [4,5,6]):
+		print(i)
+		print(element)
 		
 		
 rename_files()		
 walk_files()
+for_loop_test()
